2c4602f38b93ef35fc8233ec365037ddfc461f5c
Andrew Nichols <tandrewnichols@gmail.com>
2015-07-03 15:13:24 -0400

    Refactor common line

--- index.js
+++ index.js
@@ -3,16 +3,20 @@
     this.str = str;
   };
 
+  Consumed.prototype._updateString = function(replacement) {
+    this.str = this.str.replace(replacement, '');
+  };
+
   Consumed.prototype.consumeTill = function(c, inclusive) {
     var str = this.str.substring(0, this.str.indexOf(c) + (inclusive ? 1 : 0));
-    this.str = this.str.replace(str, '');
+    this._updateString(str);
     return str;
   };
 
   Consumed.prototype.consume = function(pattern) {
     var match = this.str.match(pattern);
     if (match) {
-      this.str = this.str.replace(pattern, '');
+      this._updateString(pattern);
       return match[0];
     }
   };
