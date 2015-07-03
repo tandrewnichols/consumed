3abe7ba006dd599584899b2329fabf0253344581
Andrew Nichols <tandrewnichols@gmail.com>
2015-07-02 21:11:36 -0400

    Break into two methods

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ module.exports = Consumer = function(str) {
   this.str = str;
 };
 
-Consumer.prototype.consume = function(pattern, inclusive) {
-  if (typeof pattern === 'string') {
-    if (pattern.length === 1) {
-      var str = this.str.substring(0, this.str.indexOf(pattern) + (inclusive ? 1 : 0));
-      this.str = this.str.replace(str, '');
-      return str;
-    } else {
-      this.str = this.str.replace(pattern, '');
-      return pattern;
-    }
-  } else {
-    var match = this.str.match(pattern);
-    if (match) {
-      this.str = this.str.replace(pattern, '');
-      return match[0];
-    }
+Consumer.prototype.consumeTill = function(c, inclusive) {
+  var str = this.str.substring(0, this.str.indexOf(c) + (inclusive ? 1 : 0));
+  this.str = this.str.replace(str, '');
+  return str;
+};
+
+Consumer.prototype.consume = function(pattern) {
+  var match = this.str.match(pattern);
+  if (match) {
+    this.str = this.str.replace(pattern, '');
+    return match[0];
   }
 };
