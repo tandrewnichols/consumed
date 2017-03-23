29c0ffbb62faf83695d705dc9e6c245b82d2fd05
Andrew Nichols <tandrewnichols@gmail.com>
2017-03-23 12:43:05 -0400

    Rebuild dist files

--- dist/consumed.js
+++ dist/consumed.js
@@ -3,20 +3,16 @@
     this.str = str;
   };
 
-  Consumed.prototype._updateString = function(replacement) {
-    this.str = this.str.replace(replacement, '');
-  };
-
   Consumed.prototype.consumeTill = function(c, inclusive) {
     var str = this.str.substring(0, this.str.indexOf(c) + (inclusive ? 1 : 0));
-    this._updateString(str);
+    this.str = this.str.substring(str.length);
     return str;
   };
 
   Consumed.prototype.consume = function(pattern) {
     var match = this.str.match(pattern);
     if (match) {
-      this._updateString(pattern);
+      this.str = this.str.replace(pattern, '');
       return match[0];
     }
   };
